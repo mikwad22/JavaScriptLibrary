530f399878cef10f6bf4f789a978fb0085778641
Mikaela 🌸 Wade <mikaelawade@Mikaelas-Air.lan>
2020-07-15 09:42:19 -0400

    07/15/20

---
+++ react-typescript-setup/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Clock from './Components/Clock';
+
+let testProp: string = 'Am I getting passed to the Clock component?'
+let optionalProp: string = 'You sure are!';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      <div className="verticalCenter">
+        <Clock testProp={testProp} optionalProp={optionalProp} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
